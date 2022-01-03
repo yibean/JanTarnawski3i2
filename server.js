eec7cea1dbf20dc2fddbe673e48b213ae4b3af1a
Janek <tarnawski.janek@gmail.com>
2022-01-03 19:46:24 +0100

    commit kolejnej zmiany

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku - mała zmiana!</h1>")
+    res.send("<h1>my app on heroku - kolejna zmiana!</h1>")
 })
 
 app.listen(PORT, function () {
