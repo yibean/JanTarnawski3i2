bdc4a6b3df15755d99f39cc9d7a5ee5b162f07cc
Janek <tarnawski.janek@gmail.com>
2022-01-03 19:33:57 +0100

    commit servera

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000; 
+// bardzo istotna linijka - port zostaje przydzielony przez Heroku
+
+app.get("/", function (req, res) {
+    res.send("<h1>my app on heroku!</h1>")
+})
+
+app.listen(PORT, function () {
+    console.log("start serwera na porcie " + PORT)
+})
